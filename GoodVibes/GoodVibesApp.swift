cefa7efc58a55f8a9c970aa53fd21315413a9bd9
uevs <36189306+uevs@users.noreply.github.com>
2022-03-31 10:53:47 +0200

    Initial Commit

---
+++ GoodVibes/GoodVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodVibesApp.swift
+//  GoodVibes
+//
+//  Created by leonardo on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GoodVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
